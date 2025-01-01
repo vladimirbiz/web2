76fc1998715b83bf880fe2a5f8273fce4dadfd6d
Vladimir Tasevski <vladimir@tasevskimarketingagency.com>
2025-01-01 03:59:46 +0100

    +

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      OVA E WEB STRANATA NA VLADO KRALOT!!!!
+    </div>
+  );
+}
+
+export default App;
